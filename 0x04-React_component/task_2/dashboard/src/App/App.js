51439914e706900bc66216dd82e0183eb66b3db3
Alejandro Pineda Sanchez <apsanchez2549@gmail.com>
2023-07-11 12:34:44 -0500

    feat: Added task 2

---
+++ 0x04-React_component/task_2/dashboard/src/App/App.js
@@ -0,0 +1,85 @@
+import './App.css';
+import { getLatestNotification } from "../utils/utils";
+import PropTypes from 'prop-types';
+import Header from '../Header/Header'
+import Login from '../Login/Login'
+import Footer from '../Footer/Footer'
+import Notifications from '../Notifications/Notifications'
+import CourseList from '../CourseList/CourseList'
+import React from 'react';
+
+export default class App extends React.Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+    logOut: PropTypes.func
+  }
+
+  static defaultProps = {
+    isLoggedIn: false,
+    logOut: function () {}
+  }
+
+  componentDidMount = () => {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+
+  render() {
+    const coursesArray = [{
+      id: 1,
+      name: 'ES6',
+      credit: 60
+    }, {
+      id: 2,
+      name: 'Webpack',
+      credit: 20
+    }, {
+      id: 3,
+      name: 'React',
+      credit: 40
+    }];
+
+    const notificationsArray = [{
+      id: 1,
+      type: 'default',
+      value: 'New course available',
+    },  {
+      id: 2,
+      type: 'urgent',
+      value: 'New resume available'
+    }, {
+      id: 3,
+      type: 'urgent',
+      html: {__html: getLatestNotification()}
+    }]
+
+    if (!this.props.isLoggedIn) {
+      return (
+        <>
+          <Notifications />
+          <Header />
+          <Login />
+          <Footer />
+        </>
+      )
+    }
+    return (
+      <>
+        <Notifications displayDrawer={true} listNotifications={notificationsArray} />
+        <Header />
+        <CourseList listCourses={coursesArray} />
+        <Footer />
+      </>
+    )
+  }
+}
---
+++ 0x04-React_component/task_2/dashboard/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App/App';
+
+const body = document.getElementsByTagName('body')[0];
+const rootDiv = document.createElement('div');
+rootDiv.classList.add("root");
+body.appendChild(rootDiv);
+
+const root = ReactDOM.createRoot(rootDiv);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
